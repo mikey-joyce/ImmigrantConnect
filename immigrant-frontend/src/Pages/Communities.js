297953401d8f551847b60c9432d0122b226abc6c
michaelimral <imralm8504@parkwayschools.net>
2018-10-13 13:02:02 -0500

    Communities and CommunityPage template

---
+++ immigrant-frontend/src/Pages/Communities.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import CommunityCard from './CommunityCard';
+
+class Communities extends Component {
+
+  render() {
+    return (
+      <div>
+        <CommunityCard />
+      </div>
+    );
+  }
+
+}
+
+export default Communities;
