f814223005caae1179b73c0cb48fa9fa28c6d794
Mechasparrow <mechasparrow@gmail.com>
2018-10-13 09:23:47 -0500

    Added Immigrant Profile Page

--- immigrantbackend/backend/models.py
+++ immigrantbackend/backend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class ImmigrantProfile (models.Model):
+    owner = models.ForeignKey('auth.User', related_name = 'immigrant_profile', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    location = models.CharField(max_length=255)
+    years_in_residence = models.IntegerField()
+    country_of_origin = models.CharField(max_length=255)
+
+    bio = models.TextField()
