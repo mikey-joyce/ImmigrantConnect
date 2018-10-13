d86d2cb58ff84dc7d3ebfce53f87ca4465e02d05
Mechasparrow <mechasparrow@gmail.com>
2018-10-13 11:45:34 -0500

    repaired user view

--- immigrantbackend/backend/views.py
+++ immigrantbackend/backend/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from rest_framework import viewsets
 from rest_framework.response import Response
