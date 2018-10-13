2d41303b7cd05312709e59ac86ca9c1e96bd78db
Mikey Joyce <joycem8845@parkwayschools.net>
2018-10-13 10:53:56 -0500

    Update immigrantbackend/backend/admin.py

--- immigrantbackend/backend/admin.py
+++ immigrantbackend/backend/admin.py
@@ -5,3 +5,10 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.ImmigrantProfile)
+admin.site.register(models.Community)
+admin.site.register(models.Language)
+admin.site.register(models.Post)
+admin.site.register(models.Event)
+admin.site.register(models.Comment)
+admin.site.register(models.LanguageConfig)
+
