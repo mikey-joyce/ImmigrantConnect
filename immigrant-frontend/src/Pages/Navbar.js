358969ec7b75cbf2e94b376108e40c8561a663d2
Mechasparrow <mechasparrow@gmail.com>
2018-10-13 13:26:30 -0500

    fixed shitty navbar

--- immigrant-frontend/src/Pages/Navbar.js
+++ immigrant-frontend/src/Pages/Navbar.js
@@ -7,16 +7,44 @@ class Navbar extends Component {
     return (
         <div className = "Navbar">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <h3>Immigrant Connect</h3>
+            <a className="navbar-brand" href="#">ImmigrantConnect</a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
 
-            <button className="btn btn-dark btn pull-right buttonRight">
-              <Link to="/sign-in">Sign In</Link>
-            </button>
-            
-            <button className="btn btn-dark btn pull-right buttonRight">
-              <Link to="/communities">Communities</Link>
-            </button>
-          </nav>
+              <li className="nav-item">
+                <Link to = "/communities" className="nav-link" >Communities</Link>
+              </li>
+
+            </ul>
+            <ul className = "navbar-nav ml-auto">
+
+              <li className="nav-item pull-right">
+                <Link to = "/sign-in" className="nav-link" >
+                  Sign In
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+
         </div>
     );
   }
