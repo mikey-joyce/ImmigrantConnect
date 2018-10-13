9491c3edad371bd1c2b20345bea4540c280cba95
Mechasparrow <mechasparrow@gmail.com>
2018-10-13 12:22:56 -0500

    rest routes and migrations

--- immigrantbackend/backend/urls.py
+++ immigrantbackend/backend/urls.py
@@ -7,6 +7,9 @@ from django.urls import path, include
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'profiles', views.ProfileViewSet)
+router.register(r'communities', views.CommunityViewSet)
+router.register(r'posts', views.PostViewSet)
+router.register(r'events', views.EventViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
