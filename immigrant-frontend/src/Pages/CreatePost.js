31c8f6ea8a0df15ca1dbf03ba86c6a706b286722
Mechasparrow <mechasparrow@gmail.com>
2018-10-13 19:21:08 -0500

    added ability to create posts

--- immigrant-frontend/src/Pages/CreatePost.js
+++ immigrant-frontend/src/Pages/CreatePost.js
@@ -7,20 +7,32 @@ import ApiInterface from '../Lib/ApiInterface'
 //models
 import models from '../Models';
 
+//routing
+import {Redirect} from 'react-router-dom';
+
 //shorthand
 let {Post} = models;
+
+
 
 class CreatePost extends Component {
 
   constructor(props) {
     super(props);
 
+    this.handleChange = this.handleChange.bind(this);
+    this.submitPost = this.submitPost.bind(this);
+
     if (this.props.community_selected && this.props.user_logged_in) {
 
       this.state = {
         valid: true,
         new_post: new Post(
-          
+          0,
+          this.props.logged_in_user.id,
+          this.props.selected_community.id,
+          "",
+          ""
         )
       }
 
@@ -34,46 +46,94 @@ class CreatePost extends Component {
 
   }
 
+  submitPost() {
+
+    let {new_post} = this.state;
+
+    ApiInterface.createPost(new_post).then (function (post) {
+      this.setState({
+        ...this.state,
+        submitted: true
+      })
+    }.bind(this))
+
+  }
+
+  handleChange(e) {
+
+    let {name, value} = e.target;
+    let {new_post} = this.state;
+
+    new_post[name] = value;
+
+    this.setState({
+      ...this.state,
+      new_post
+    })
+
+  }
+
   render() {
-    return (
-      <div className = "container-fluid">
-        <form>
 
-          <div className = "form-group">
-            <label>
-              Post Title:
+    if (this.state.valid === false) {
+      return (
+        <Redirect to = "/communities"></Redirect>
+      )
+    }
 
-            </label>
+    if (this.state.submitted == true) {
+      return (
+        <Redirect to = "/view-community"></Redirect>
+      )
+    }
 
-            <input
-              type="text"
-              placeholder="Add a title"
-              className="form-control"
-              name="post-title"
-            />
+    if (this.state.valid) {
+
+      return (
+        <div className = "container-fluid">
+          <div>
+
+            <div className = "form-group">
+              <label>
+                Post Title:
+
+              </label>
+
+              <input
+                type="text"
+                placeholder="Add a title"
+                onChange = {this.handleChange}
+                className="form-control"
+                value = {this.state.new_post.title}
+                name="title"
+              />
+            </div>
+
+            <div className = "form-group">
+              <label>
+                Contents:
+              </label>
+
+              <textarea
+                type="text"
+                onChange = {this.handleChange}
+                placeholder="Add post contents"
+                className="form-control"
+                value = {this.state.new_post.text}
+                name="text"
+              />
+
+            </div>
+
+            <button
+              type="submit"
+              value="Submit"
+              onClick = {this.submitPost}
+              className="btn btn-primary">Submit</button>
           </div>
-
-          <div className = "form-group">
-            <label>
-              Contents:
-            </label>
-
-            <textarea
-              type="text"
-              placeholder="Add post contents"
-              className="form-control"
-              name="post-contents"
-            />
-
-          </div>
-
-          <button
-            type="submit"
-            value="Submit"
-            className="btn btn-primary">Submit</button>
-        </form>
-      </div>
-    );
+        </div>
+      );
+    }
   }
 }
 
